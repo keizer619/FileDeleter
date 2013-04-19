d8deedb504f3b49397f1100466b28aaec5b44c4c
keizer619 <onestepfromhell@hotmail.com>
2013-04-19 12:43:05 +0530

    Files Added

---
+++ FileDeleter/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace FileDeleter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(356, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(82, 41);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtList
+            // 
+            this.txtList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtList.Location = new System.Drawing.Point(15, 111);
+            this.txtList.Multiline = true;
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(423, 436);
+            this.txtList.TabIndex = 1;
+            // 
+            // txtBase
+            // 
+            this.txtBase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBase.Location = new System.Drawing.Point(12, 72);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(424, 20);
+            this.txtBase.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Base URL :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "URL List :";
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonChange.Location = new System.Drawing.Point(238, 12);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(112, 41);
+            this.buttonChange.TabIndex = 5;
+            this.buttonChange.Text = "Change \"/\" to \"\\\"";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.buttonChange_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 559);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.btnDelete);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Untrack File Deleter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonChange;
+    }
+}
+
---
+++ FileDeleter/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileDeleter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            if  (txtBase.Text.Trim() != String.Empty)
+            {
+                string url = txtBase.Text.Trim()+@"\";
+                
+                ArrayList arrList = new ArrayList();
+                arrList.AddRange(txtList.Text.Split('\n'));
+
+                string exc = "";
+                string nonDeleted = "";
+
+                int i = 0;
+                int nonDelCount = 0;
+                for (; i < arrList.Count; i++)
+                {
+
+                    string curURL = url + arrList[i];
+
+                    if (curURL.Trim() != String.Empty)
+                    {
+                        if (System.IO.File.Exists(curURL))
+                        {
+                            try
+                            {
+                                System.IO.File.Delete(curURL);
+                                arrList.Remove(i);
+                            }
+                            catch (Exception ex)
+                            {
+                                nonDelCount++;
+                                exc += ex.Message + " : " + curURL + "\n";
+                                nonDeleted += curURL + "\n";
+                            }
+
+                        }
+                        else
+                        {
+                           nonDelCount++;
+                           nonDeleted += curURL + "\n";
+                            
+                        }
+                    }
+                 
+                }
+
+                MessageBox.Show(i - nonDelCount + " File(s) Deleted", "File Deletion",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+
+                if (exc != "")
+                {
+                    txtList.Text += exc+"\n \n *******************************************************";
+                }
+                if (nonDelCount > 0)
+                {
+                    txtList.Text = "Following files did not get deleted : \n" + nonDeleted;
+                }
+                else
+                {
+                    txtList.Text = "";
+                }
+            }
+        }
+
+        private void buttonChange_Click(object sender, EventArgs e)
+        {
+            txtList.Text = txtList.Text.Replace("/", @"\");
+        }
+    }
+}
